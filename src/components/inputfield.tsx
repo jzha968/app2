319dec50d1f526a6f089017d9973878287fd0503
jzha968 <53927183+jzha968@users.noreply.github.com>
2022-08-08 15:07:58 +1200

    components
    basic components

---
+++ src/components/inputfield.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { TextField } from '@mui/material';
+
+export default function SearchBar(){
+return(
+    <div style={{marginTop : '30px'}}>
+<TextField id="outlined-basic" label="Outlined" variant="outlined" />
+</div>
+)
+
+}
+
